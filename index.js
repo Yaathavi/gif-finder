f54c5bb52845a03f409c989fcd9727570cfbb6fd
Yaathavi <yaathavi@gmail.com>
2021-01-10 19:31:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const APIToken = process.env.API_KEY;
 const GIFKEY = process.env.GIFKEY;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
