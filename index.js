e6811c988ece3a810f1a732861f2ae4d87063d30
Yaathavi <yaathavi@gmail.com>
2021-01-10 19:39:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Initialize client
+// Initialize the client
 const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 
 const stub = ClarifaiStub.grpc();
